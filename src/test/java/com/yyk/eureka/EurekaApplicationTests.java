c167bd200e93fb200ecfea4ceb13d24b0faee003
yyksin@naver.com <did6811!>
2021-01-09 20:33:24 +0900

    project source

---
+++ src/test/java/com/yyk/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yyk.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
